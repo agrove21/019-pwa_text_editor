9dc624ba4466c86275dcc2bc0760a82763ddae01
Your Name <you@example.com>
2024-11-03 10:43:32 -0600

    updated all the todos

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,33 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent Chrome 76 and later from showing the mini-infobar
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Show the install button
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (!window.deferredPrompt) {
+        return;
+    }
+    // Show the install prompt
+    window.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await window.deferredPrompt.userChoice;
+    // Hide the install button
+    butInstall.classList.toggle('hidden', true);
+    // Clear the deferredPrompt
+    window.deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Log the installation to the analytics
+    console.log('Jate was installed', event);
+    window.deferredPrompt = null;
+});

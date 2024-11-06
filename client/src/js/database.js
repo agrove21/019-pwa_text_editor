1c8406ef11ecc82f410a5d1853665c1684e20ad3
Your Name <you@example.com>
2024-11-06 08:41:58 -0600

    debugged indexdb adding and reading data.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,32 +1,38 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  await store.put({ content });
-  await tx.done;
-}
+  try {
+    const db = await openDB("jate", 1);
+    const tx = db.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = await store.put({ id: 1, content });
+    const result = await request;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  return store.getAll();
-}
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  return result.content;
+};
 
 initdb();
